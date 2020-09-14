a684d0347a6288cf208481132d303fbd922b48c8
unknown <Mohammad Osaama@nahe-osaama.isb.hec.gov.pk>
2020-09-14 14:18:24 +0500

    added cohort 3 graduates

--- certificates/utils.py
+++ certificates/utils.py
@@ -2,15 +2,13 @@
 from certificates.models import Fellow
 import pandas as pd 
 
-master = pd.read_excel (r"C:\Users\Administrator\OneDrive - Higher Education Commission\Applications\Encryption Record-3.0.xlsx")
-allowed = ["JUN-JUL20", "APR-MAY20"]
+master = pd.read_excel (r"C:\Users\Administrator\OneDrive - Higher Education Commission\Applications\NFDP Successful Fellows (All Cohorts).xlsx")
 
 for fellow in range (len(master)):
     name = master.iloc[fellow, master.columns.get_loc ("NAME OF IPFP FELLOW")]
     cnic = master.iloc[fellow, master.columns.get_loc ("CNIC")]
     program =  master.iloc[fellow, master.columns.get_loc ("NFDP Cohort Month")]
-    if program in allowed: 
-        new = Fellow (name=name, CNIC=cnic, program=program)
-        new.save()
+    new = Fellow (name=name, CNIC=cnic, program=program)
+    new.save()
 
 
