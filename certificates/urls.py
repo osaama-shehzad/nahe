ea41388432c25e16149b33228b789945b7cb177f
unknown <Mohammad Osaama@nahe-osaama.isb.hec.gov.pk>
2020-09-16 17:51:34 +0500

    collect static

--- certificates/urls.py
+++ certificates/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path("", views.index, name="index"), 
     path("download/<str:name>/<str:date>", views.download, name="download")
 
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
