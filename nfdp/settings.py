d83020d661f0050fb56d029fb07f01e78f7881e3
unknown <Mohammad Osaama@nahe-osaama.isb.hec.gov.pk>
2020-09-18 23:15:08 +0500

    added transcript form

--- nfdp/settings.py
+++ nfdp/settings.py
@@ -145,5 +145,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'nahe.hec@gmail.com'
-EMAIL_HOST_PASSWORD = 'nahe1234'
+EMAIL_HOST_PASSWORD = 'OSDpw56@'
 EMAIL_PORT = 587
