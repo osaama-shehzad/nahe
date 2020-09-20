f8db5986295091a3f06240ae7090cb16470c3c31
unknown <Mohammad Osaama@nahe-osaama.isb.hec.gov.pk>
2020-09-20 14:14:56 +0500

    env variable

--- nfdp/settings.py
+++ nfdp/settings.py
@@ -147,5 +147,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'nahe.hec@gmail.com'
-EMAIL_HOST_PASSWORD = 'OSDpw56@'
+EMAIL_HOST_USER = os.environ.get('NAHE-USER')
+EMAIL_HOST_PASSWORD = os.environ.get('NAHE-PASS')
