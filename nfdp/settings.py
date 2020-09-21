6b6bd4ccfbb0ecbc49b240d54be266bb78f2e531
unknown <Mohammad Osaama@nahe-osaama.isb.hec.gov.pk>
2020-09-21 17:42:04 +0500

    added warning note in transcript request

--- nfdp/settings.py
+++ nfdp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '70-=4h!m*_y+mp!w)x1&k0qqu0icb*ik-))whg12&5^615+s-c'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
